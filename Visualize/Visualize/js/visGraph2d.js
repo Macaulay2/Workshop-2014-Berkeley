cd0baab22194cfe7c014ca4fec44299a9a735738
bbarwick <bbarwick@uscupstate.edu>
2014-01-10 16:13:43 -0500

    edits in visgraph.js

--- Visualize/Visualize/js/visGraph2d.js
+++ Visualize/Visualize/js/visGraph2d.js
@@ -40,11 +40,8 @@ function initializeBuilder() {
     .append('svg')
     .attr('width', width)
     .attr('height', height)
-<<<<<<< HEAD
     .attr('id', 'canvasElement2d');
-=======
     .attr('id', 'canvasElement');
->>>>>>> 6a2814f51ee0d042c7cfe772ca30b0fcee9d0fdf
 
   // set up initial nodes and links
   //  - nodes are known by 'id', not by index in array.
@@ -69,17 +66,16 @@ function initializeBuilder() {
       }
   }
 
-<<<<<<< HEAD
-=======
   var maxLength = d3.max(nodes, function(d) {
     return d.name.length;
   });
 
   if(maxLength < 4){
     d3.selectAll("text").classed("fill", #FEFCFF);
+  } else { 
+    d3.selectAll("text").classed("fill", #000000);
   }
 
->>>>>>> 6a2814f51ee0d042c7cfe772ca30b0fcee9d0fdf
   constrString = graph2M2Constructor(nodes,links);
   incMatrix = getIncidenceMatrix(nodes,links);
   adjMatrix = getAdjacencyMatrix(nodes,links);
@@ -358,6 +354,18 @@ function restart() {
         d3.select(this.parentNode).select("text").text(function(d) {return d.name});
       }
 
+      document.getElementById("constructorString").innerHTML = "Macaulay2 Constructor: " + graph2M2Constructor(nodes,links);
+
+      var maxLength = d3.max(nodes, function(d) {
+        return d.name.length;
+      });
+      
+      if(maxLength < 4){
+        d3.selectAll("text").classed("fill", #FEFCFF);
+      } else { 
+        d3.selectAll("text").classed("fill", #000000);
+      }
+
     });
 
   // show node IDs
@@ -568,11 +576,8 @@ function setAllNodesFixed() {
 
 function updateWindowSize2d() {
 
-<<<<<<< HEAD
         var svg = document.getElementById("canvasElement2d");
-=======
         var svg = document.getElementById("canvasElement");
->>>>>>> 6a2814f51ee0d042c7cfe772ca30b0fcee9d0fdf
         svg.style.width = window.innerWidth;
         svg.style.height = window.innerHeight - 150;
         svg.width = window.innerWidth;
